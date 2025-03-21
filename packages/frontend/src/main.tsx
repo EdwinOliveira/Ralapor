a54c393adc20c54cebccf1d28dc262da110858b4
Diogo Oliveira <diogorafaellourencooliveira97@gmail.com>
2025-03-21 16:16:40 +0000

    WIP: Updated code

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -3,8 +3,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App.tsx";
-import CreateUser from "./pages/CreateUser.tsx";
-import AccessUser from "./pages/AccessUser.tsx";
 
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 createRoot(document.getElementById("root")!).render(
@@ -12,8 +10,6 @@ createRoot(document.getElementById("root")!).render(
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<App />} />
-				<Route path="/create-user" element={<CreateUser />} />
-				<Route path="/access-user" element={<AccessUser />} />
 			</Routes>
 		</BrowserRouter>
 	</StrictMode>,
