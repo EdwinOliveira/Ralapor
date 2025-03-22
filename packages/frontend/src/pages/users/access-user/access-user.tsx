75546e70e280e24fb09cb1f6734c033dc51c5a27
Diogo Oliveira <diogorafaellourencooliveira97@gmail.com>
2025-03-22 12:57:59 +0000

    WIP: updated code

--- packages/frontend/src/pages/users/access-user/access-user.tsx
+++
@@ -1,5 +0,0 @@
-import "./access-user.css";
-
-export default function AccessUser() {
-	return <div id="access-user" />;
-}
--- packages/frontend/src/pages/users/create-user/create-user.tsx
+++
@@ -1,5 +0,0 @@
-import "./create-user.css";
-
-export default function CreateUser() {
-	return <div id="create-user" />;
-}
