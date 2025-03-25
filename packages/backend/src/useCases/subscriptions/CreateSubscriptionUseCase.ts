1eab519d2fc8ea24aaa5d198f0ad6acafe803497
Diogo Oliveira <diogorafaellourencooliveira97@gmail.com>
2025-03-25 15:18:00 +0000

    WIP: Worked on subscription

--- packages/backend/src/useCases/subscriptions/CreateSubscriptionUseCase.ts
+++ packages/backend/src/useCases/subscriptions/CreateSubscriptionUseCase.ts
@@ -4,9 +4,19 @@ import type {
 } from "../../domains/Subscription";
 import { SubscriptionRemoteRepository } from "../../repositories/SubscriptionRemoteRepository";
 import type { UseCaseRequest, UseCaseResponse } from "../../types/UseCase";
+import { FindBookByIdUseCase } from "../books/FindBookByIdUseCase";
+import { FindChapterByIdUseCase } from "../chapters/FindChapterByIdUseCase";
+import { FindDossierByIdUseCase } from "../dossiers/FindDossierByIdUseCase";
+import { FindPageByIdUseCase } from "../pages/FindPageByIdUseCase";
+import { UpdateWalletByIdUseCase } from "../wallets/UpdateWalletByIdUseCase";
 
 const CreateSubscriptionUseCase = () => {
 	const repository = SubscriptionRemoteRepository();
+	const { findDossierById } = FindDossierByIdUseCase();
+	const { findBookById } = FindBookByIdUseCase();
+	const { findChapterById } = FindChapterByIdUseCase();
+	const { findPageById } = FindPageByIdUseCase();
+	const { updateWalletById } = UpdateWalletByIdUseCase();
 
 	return {
 		createSubscription: async ({
@@ -32,6 +42,56 @@ const CreateSubscriptionUseCase = () => {
 
 			if (createdSubscriptionsId.length === 0) {
 				return { statusCode: 404 };
+			}
+
+			let price = 0;
+
+			if (dossierId !== undefined) {
+				const { statusCode, args } = await findDossierById({
+					schemaArgs: { params: { id: dossierId } },
+				});
+
+				if (statusCode === 200 && args !== undefined) {
+					price += args.price;
+				}
+			}
+
+			if (bookId !== undefined) {
+				const { statusCode, args } = await findBookById({
+					schemaArgs: { params: { id: bookId } },
+				});
+
+				if (statusCode === 200 && args !== undefined) {
+					price += args.price;
+				}
+			}
+
+			if (chapterId !== undefined) {
+				const { statusCode, args } = await findChapterById({
+					schemaArgs: { params: { id: chapterId } },
+				});
+
+				if (statusCode === 200 && args !== undefined) {
+					price += args.price;
+				}
+			}
+
+			if (pageId !== undefined) {
+				const { statusCode, args } = await findPageById({
+					schemaArgs: { params: { id: pageId } },
+				});
+
+				if (statusCode === 200 && args !== undefined) {
+					price += args.price;
+				}
+			}
+
+			const { statusCode } = await updateWalletById({
+				schemaArgs: { params: { id: walletId }, body: { funds: price } },
+			});
+
+			if (statusCode !== 201) {
+				return { statusCode: 500 };
 			}
 
 			return {
