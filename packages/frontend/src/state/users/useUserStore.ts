9cbb44b54ccb5cb2c0cdebe73f78b39ed65fb5c9
Diogo Oliveira <39379560+EdwinOliveira@users.noreply.github.com>
2025-05-05 17:38:23 +0100

    WIP: Updated code around stores

--- packages/frontend/src/state/users/useUserStore.ts
+++ packages/frontend/src/state/users/useUserStore.ts
@@ -15,24 +15,23 @@ type User = {
 
 type UserStoreSignature = {
 	users: User[];
-	addUser: (user: User) => void;
-	updateUserById: (query: Pick<User, "id">, args: Partial<User>) => void;
+	addUser: (args: User) => void;
+	updateUserById: (
+		query: Pick<User, "id">,
+		args: Partial<Omit<User, "id" | "createdAt">>,
+	) => void;
 };
 
 const useUserStore = create<UserStoreSignature>((set) => {
 	return {
 		users: [],
-		addUser: (user) => {
-			return set((state) => ({ users: [...state.users, user] }));
+		addUser: (args) => {
+			return set((state) => ({ users: [...state.users, args] }));
 		},
 		updateUserById: (query, args) => {
 			return set((state) => ({
 				users: state.users.map((user) => {
-					if (user.id === query.id) {
-						user = { ...user, ...args };
-					}
-
-					return user;
+					return user.id === query.id ? { ...user, ...args } : user;
 				}),
 			}));
 		},
