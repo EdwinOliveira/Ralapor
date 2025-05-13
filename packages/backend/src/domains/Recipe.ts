cd31ccb1fed756d819b4ddba2b41199ccf4f1f2f
Diogo Oliveira <39379560+EdwinOliveira@users.noreply.github.com>
2025-05-13 13:58:50 +0100

    WIP: Updated recipe

--- packages/backend/src/domains/Recipe.ts
+++ packages/backend/src/domains/Recipe.ts
@@ -8,3 +8,16 @@ type RecipeEntity = {
 };
 
 type RecipeDTO = Readonly<RecipeEntity>;
+
+const recipeDTOMapper = (entity: RecipeEntity): RecipeDTO => {
+	return {
+		id: entity.id,
+		designation: entity.designation,
+		description: entity.description,
+		dietaryPattern: entity.dietaryPattern,
+		createdAt: entity.createdAt,
+		updatedAt: entity.updatedAt,
+	};
+};
+
+export { type RecipeEntity, type RecipeDTO, recipeDTOMapper };
