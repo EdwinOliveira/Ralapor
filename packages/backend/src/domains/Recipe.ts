8f2bc17736b0257acc0c97fc3a4550aae6a91b52
Diogo Oliveira <39379560+EdwinOliveira@users.noreply.github.com>
2025-05-13 15:57:03 +0100

    WIP: Updated code around recipe

--- packages/backend/src/domains/Recipe.ts
+++ packages/backend/src/domains/Recipe.ts
@@ -1,3 +1,9 @@
+import { z } from "zod";
+import type {
+	RepositoryRequest,
+	RepositoryResponse,
+} from "../signatures/Repository";
+
 type RecipeEntity = {
 	id: number;
 	designation: string;
@@ -20,4 +26,92 @@ const recipeDTOMapper = (entity: RecipeEntity): RecipeDTO => {
 	};
 };
 
-export { type RecipeEntity, type RecipeDTO, recipeDTOMapper };
+const findRecipesByQuerySchema = z.object({
+	query: z.object({
+		dietaryPattern: z.enum(["plant-based", "animal-based"]).optional(),
+	}),
+});
+
+type FindRecipesByQueryRequest = z.infer<typeof findRecipesByQuerySchema>;
+
+const findRecipeByIdSchema = z.object({
+	params: z.object({
+		id: z
+			.string()
+			.transform((id) => Number.parseInt(id))
+			.refine((id) => !Number.isNaN(id)),
+	}),
+});
+
+type FindRecipeByIdRequest = z.infer<typeof findRecipeByIdSchema>;
+
+const createRecipeSchema = z.object({
+	body: z.object({
+		designation: z.string(),
+		description: z.string(),
+		dietaryPattern: z.enum(["plant-based", "animal-based"]),
+	}),
+});
+
+type CreateRecipeRequest = z.infer<typeof createRecipeSchema>;
+
+const updateRecipeByIdSchema = z.object({
+	params: z.object({
+		id: z
+			.string()
+			.transform((id) => Number.parseInt(id))
+			.refine((id) => !Number.isNaN(id)),
+	}),
+	body: z.object({
+		designation: z.string().optional(),
+		description: z.string().optional(),
+		dietaryPattern: z.enum(["plant-based", "animal-based"]).optional(),
+	}),
+});
+
+type UpdateRecipeByIdRequest = z.infer<typeof updateRecipeByIdSchema>;
+
+interface RecipeRepository {
+	findRecipes({
+		query,
+	}: RepositoryRequest<Partial<Pick<RecipeEntity, "dietaryPattern">>>): Promise<
+		RepositoryResponse<RecipeEntity>
+	>;
+	findRecipeByDesignation({
+		query,
+	}: RepositoryRequest<Pick<RecipeEntity, "designation">>): Promise<
+		RepositoryResponse<RecipeEntity>
+	>;
+	findRecipeById({
+		query,
+	}: RepositoryRequest<Pick<RecipeEntity, "id">>): Promise<
+		RepositoryResponse<RecipeEntity>
+	>;
+	createRecipe({
+		args,
+	}: RepositoryRequest<
+		unknown,
+		Pick<RecipeEntity, "designation" | "description" | "dietaryPattern">
+	>): Promise<RepositoryResponse<unknown>>;
+	updateRecipeById({
+		query,
+		args,
+	}: RepositoryRequest<
+		Pick<RecipeEntity, "id">,
+		Partial<Omit<RecipeEntity, "id" | "createdAt" | "updatedAt">>
+	>): Promise<RepositoryResponse<Pick<RecipeEntity, "updatedAt">>>;
+}
+
+export {
+	type RecipeEntity,
+	type RecipeDTO,
+	recipeDTOMapper,
+	findRecipesByQuerySchema,
+	type FindRecipesByQueryRequest,
+	type FindRecipeByIdRequest,
+	createRecipeSchema,
+	type CreateRecipeRequest,
+	updateRecipeByIdSchema,
+	type UpdateRecipeByIdRequest,
+	type RecipeRepository,
+};
