9feb171bed0f64d07631da3a9598cec3e3c6e9ef
Diogo Oliveira <S02666@newday.co.uk>
2025-06-01 23:14:03 +0100

    WIP: Added Todo Comment

--- packages/backend/src/routers/UserRouter.ts
+++ packages/backend/src/routers/UserRouter.ts
@@ -6,13 +6,13 @@ import { UpdateUserByIdUseCase } from "../useCases/users/UpdateUserByIdUseCase";
 import { UpdateUserAccessCodeByIdUseCase } from "../useCases/users/UpdateUserAccessCodeByIdUseCase";
 import { UpdateUserAccessCodeByUsernameOrEmailOrPhoneNumberUseCase } from "../useCases/users/UpdateUserAccessCodeByUsernameOrEmailOrPhoneNumberUseCase";
 import {
-	createUserSchema,
-	deleteUserSessionByIdSchema,
-	findUserByAccessCodeSchema,
-	findUserByIdSchema,
-	updateUserAccessCodeByIdSchema,
-	updateUserAccessCodeByUsernameOrEmailOrPhoneNumberSchema,
-	updateUserByIdSchema,
+  createUserSchema,
+  deleteUserSessionByIdSchema,
+  findUserByAccessCodeSchema,
+  findUserByIdSchema,
+  updateUserAccessCodeByIdSchema,
+  updateUserAccessCodeByUsernameOrEmailOrPhoneNumberSchema,
+  updateUserByIdSchema,
 } from "../domains/User";
 import { SessionProvider } from "../providers/SessionProvider";
 import { SessionGuard } from "../guards/SessionGuard";
@@ -23,293 +23,295 @@ import { TokenProvider } from "../providers/TokenProvider";
 import { RateLimitProvider } from "../providers/RateLimitProvider";
 
 const UserRouter = () => {
-	const { isAuthenticated, isAuthenticating } = SessionGuard();
-	const { createRateLimit } = RateLimitProvider();
+  const { isAuthenticated, isAuthenticating } = SessionGuard();
+  const { createRateLimit } = RateLimitProvider();
 
-	const subscribe = (router: Router): Router => {
-		router.get(
-			"/sessions",
-			isAuthenticated,
-			async (request: Request, response: Response) => {
-				const { getSession } = SessionProvider(request, response);
-				const session = getSession();
+  const subscribe = (router: Router): Router => {
+    router.get(
+      "/sessions",
+      isAuthenticated,
+      async (request: Request, response: Response) => {
+        const { getSession } = SessionProvider(request, response);
+        const session = getSession();
 
-				const { findOnCache } = CacheService();
-				const foundSession = await findOnCache(`session:${session?.sid}`);
+        const { findOnCache } = CacheService();
+        const foundSession = await findOnCache(`session:${session?.sid}`);
 
-				/**
-				 * Guard will validate the user session.
-				 * This endpoint only exists to be called so the guard does the validation work.
-				 */
-				return void response.status(200).json(foundSession);
-			},
-		);
+        /**
+         * Guard will validate the user session.
+         * This endpoint only exists to be called so the guard does the validation work.
+         */
+        return void response.status(200).json(foundSession);
+      }
+    );
 
-		router.get(
-			"/:id",
-			isAuthenticated,
-			async (request: Request, response: Response) => {
-				const { data: schemaArgs, error: schemaErrors } =
-					findUserByIdSchema.safeParse({ params: request.params });
+    router.get(
+      "/:id",
+      isAuthenticated,
+      async (request: Request, response: Response) => {
+        const { data: schemaArgs, error: schemaErrors } =
+          findUserByIdSchema.safeParse({ params: request.params });
 
-				if (schemaErrors !== undefined) {
-					return void response
-						.status(400)
-						.json({ errors: schemaErrors.issues });
-				}
+        if (schemaErrors !== undefined) {
+          return void response
+            .status(400)
+            .json({ errors: schemaErrors.issues });
+        }
 
-				const { findUserById } = FindUserByIdUseCase();
-				const { statusCode, args } = await findUserById({
-					schemaArgs,
-				});
+        const { findUserById } = FindUserByIdUseCase();
+        const { statusCode, args } = await findUserById({
+          schemaArgs,
+        });
 
-				return void response.status(statusCode).json(args);
-			},
-		);
+        return void response.status(statusCode).json(args);
+      }
+    );
 
-		router.post(
-			"/access-code",
-			isAuthenticating,
-			createRateLimit(15 * 60 * 1000, 5),
-			async (request: Request, response: Response) => {
-				const { data: schemaArgs, error: schemaErrors } =
-					findUserByAccessCodeSchema.safeParse({ body: request.body });
+    router.post(
+      "/access-code",
+      isAuthenticating,
+      createRateLimit(15 * 60 * 1000, 5),
+      async (request: Request, response: Response) => {
+        const { data: schemaArgs, error: schemaErrors } =
+          findUserByAccessCodeSchema.safeParse({ body: request.body });
 
-				if (schemaErrors !== undefined) {
-					return void response
-						.status(400)
-						.json({ errors: schemaErrors.issues });
-				}
+        if (schemaErrors !== undefined) {
+          return void response
+            .status(400)
+            .json({ errors: schemaErrors.issues });
+        }
 
-				const { findUserByAccessCode } = FindUserByAccessCodeUseCase();
-				const { statusCode, args } = await findUserByAccessCode({
-					schemaArgs,
-				});
+        const { findUserByAccessCode } = FindUserByAccessCodeUseCase();
+        const { statusCode, args } = await findUserByAccessCode({
+          schemaArgs,
+        });
 
-				if (args === undefined) {
-					return void response.status(statusCode).json();
-				}
+        if (args === undefined) {
+          return void response.status(statusCode).json();
+        }
 
-				const randomProvider = RandomProvider();
-				const sessionProvider = SessionProvider(request, response);
-				const cacheService = CacheService();
-				const tokenProvider = TokenProvider();
+        const randomProvider = RandomProvider();
+        const sessionProvider = SessionProvider(request, response);
+        const cacheService = CacheService();
+        const tokenProvider = TokenProvider();
 
-				const sessionId = randomProvider.createRandomUuid();
+        const sessionId = randomProvider.createRandomUuid();
 
-				const refreshToken = await tokenProvider.createToken(
-					{ sessionId },
-					"7d",
-					process.env.SESSION_TOKEN_SECRET,
-				);
+        const refreshToken = await tokenProvider.createToken(
+          { sessionId },
+          "7d",
+          process.env.SESSION_TOKEN_SECRET
+        );
 
-				if (refreshToken === undefined) {
-					return void response.status(500).json();
-				}
+        if (refreshToken === undefined) {
+          return void response.status(500).json();
+        }
 
-				sessionProvider.addToSession(sessionId, schemaArgs.body.rememberDevice);
+        sessionProvider.addToSession(sessionId, schemaArgs.body.rememberDevice);
 
-				await cacheService.addToCache(`session:${sessionId}`, {
-					sessionId: sessionId,
-					userId: args.id,
-					roleId: args.roleId,
-					expiresIn: new Date().setSeconds(
-						randomProvider.createExpirationTime(),
-					),
-					refreshToken,
-					deviceUuid: schemaArgs.body.rememberDevice ? crypto.randomUUID() : "",
-				});
+        await cacheService.addToCache(`session:${sessionId}`, {
+          sessionId: sessionId,
+          userId: args.id,
+          roleId: args.roleId,
+          expiresIn: new Date().setSeconds(
+            randomProvider.createExpirationTime()
+          ),
+          refreshToken,
+          deviceUuid: schemaArgs.body.rememberDevice ? crypto.randomUUID() : "",
+        });
 
-				await cacheService.addToCache(
-					`authentication_code:session:${sessionId}`,
-					{ code: randomProvider.createRandomString(4) },
-					300,
-				);
+        await cacheService.addToCache(
+          `authentication_code:session:${sessionId}`,
+          { code: randomProvider.createRandomString(4) },
+          300
+        );
 
-				return void response.status(statusCode).json(args);
-			},
-		);
+        // TODO: Send MFA Code To Client By Email or Phone Number
 
-		router.post(
-			"/",
-			createRateLimit(15 * 60 * 1000, 5),
-			async (request: Request, response: Response) => {
-				const { data: schemaArgs, error: schemaErrors } =
-					createUserSchema.safeParse({ body: request.body });
+        return void response.status(statusCode).json(args);
+      }
+    );
 
-				if (schemaErrors !== undefined) {
-					return void response
-						.status(400)
-						.json({ errors: schemaErrors.issues });
-				}
+    router.post(
+      "/",
+      createRateLimit(15 * 60 * 1000, 5),
+      async (request: Request, response: Response) => {
+        const { data: schemaArgs, error: schemaErrors } =
+          createUserSchema.safeParse({ body: request.body });
 
-				const { createUser } = CreateUserUseCase();
-				const { statusCode, args } = await createUser({
-					schemaArgs,
-				});
+        if (schemaErrors !== undefined) {
+          return void response
+            .status(400)
+            .json({ errors: schemaErrors.issues });
+        }
 
-				return void response.status(statusCode).json({ id: args?.id });
-			},
-		);
+        const { createUser } = CreateUserUseCase();
+        const { statusCode, args } = await createUser({
+          schemaArgs,
+        });
 
-		router.put(
-			"/:id",
-			isAuthenticated,
-			async (request: Request, response: Response) => {
-				const { data: schemaArgs, error: schemaErrors } =
-					updateUserByIdSchema.safeParse({
-						params: request.params,
-						body: request.body,
-					});
+        return void response.status(statusCode).json({ id: args?.id });
+      }
+    );
 
-				if (schemaErrors !== undefined) {
-					return void response
-						.status(400)
-						.json({ errors: schemaErrors.issues });
-				}
+    router.put(
+      "/:id",
+      isAuthenticated,
+      async (request: Request, response: Response) => {
+        const { data: schemaArgs, error: schemaErrors } =
+          updateUserByIdSchema.safeParse({
+            params: request.params,
+            body: request.body,
+          });
 
-				const { updateUserById } = UpdateUserByIdUseCase();
-				const { statusCode, args } = await updateUserById({
-					schemaArgs,
-				});
+        if (schemaErrors !== undefined) {
+          return void response
+            .status(400)
+            .json({ errors: schemaErrors.issues });
+        }
 
-				return void response.status(statusCode).json({
-					id: args?.id,
-					updatedAt: args?.updatedAt,
-				});
-			},
-		);
+        const { updateUserById } = UpdateUserByIdUseCase();
+        const { statusCode, args } = await updateUserById({
+          schemaArgs,
+        });
 
-		router.put(
-			"/:id/access-code",
-			isAuthenticated,
-			async (request: Request, response: Response) => {
-				const { data: schemaArgs, error: schemaErrors } =
-					updateUserAccessCodeByIdSchema.safeParse({ params: request.params });
+        return void response.status(statusCode).json({
+          id: args?.id,
+          updatedAt: args?.updatedAt,
+        });
+      }
+    );
 
-				if (schemaErrors !== undefined) {
-					return void response
-						.status(400)
-						.json({ errors: schemaErrors.issues });
-				}
+    router.put(
+      "/:id/access-code",
+      isAuthenticated,
+      async (request: Request, response: Response) => {
+        const { data: schemaArgs, error: schemaErrors } =
+          updateUserAccessCodeByIdSchema.safeParse({ params: request.params });
 
-				const { updateUserAccessCodeById } = UpdateUserAccessCodeByIdUseCase();
-				const { statusCode, args } = await updateUserAccessCodeById({
-					schemaArgs,
-				});
+        if (schemaErrors !== undefined) {
+          return void response
+            .status(400)
+            .json({ errors: schemaErrors.issues });
+        }
 
-				const sessionProvider = SessionProvider(request, response);
-				const cookies = sessionProvider.getSession();
+        const { updateUserAccessCodeById } = UpdateUserAccessCodeByIdUseCase();
+        const { statusCode, args } = await updateUserAccessCodeById({
+          schemaArgs,
+        });
 
-				if (args === undefined || cookies?.sid === undefined) {
-					return void response.status(500).json();
-				}
+        const sessionProvider = SessionProvider(request, response);
+        const cookies = sessionProvider.getSession();
 
-				const { findOnCache, addToCache, updateOnCache } = CacheService();
-				const { createRandomUuid, createExpirationTime } = RandomProvider();
-				const { createToken } = TokenProvider();
+        if (args === undefined || cookies?.sid === undefined) {
+          return void response.status(500).json();
+        }
 
-				const refreshToken = await createToken(
-					{ sessionId: cookies.sid },
-					"7d",
-					process.env.SESSION_TOKEN_SECRET,
-				);
+        const { findOnCache, addToCache, updateOnCache } = CacheService();
+        const { createRandomUuid, createExpirationTime } = RandomProvider();
+        const { createToken } = TokenProvider();
 
-				if (refreshToken === undefined) {
-					return void response.status(500).json();
-				}
+        const refreshToken = await createToken(
+          { sessionId: cookies.sid },
+          "7d",
+          process.env.SESSION_TOKEN_SECRET
+        );
 
-				const foundSession = await findOnCache(`session:${cookies.sid}`);
+        if (refreshToken === undefined) {
+          return void response.status(500).json();
+        }
 
-				if (foundSession === undefined) {
-					await addToCache(`session:${cookies.sid}`, {
-						sessionId: cookies.sid,
-						userId: args.id,
-						roleId: args.roleId,
-						expiresIn: createExpirationTime(),
-						refreshToken,
-						deviceUuid: createRandomUuid(),
-					});
-				} else {
-					await updateOnCache(`session:${cookies.sid}`, {
-						...foundSession,
-						expiresIn: createExpirationTime(),
-						refreshToken,
-					});
-				}
+        const foundSession = await findOnCache(`session:${cookies.sid}`);
 
-				return void response.status(statusCode).json({
-					id: args?.id,
-					updatedAt: args?.updatedAt,
-				});
-			},
-		);
+        if (foundSession === undefined) {
+          await addToCache(`session:${cookies.sid}`, {
+            sessionId: cookies.sid,
+            userId: args.id,
+            roleId: args.roleId,
+            expiresIn: createExpirationTime(),
+            refreshToken,
+            deviceUuid: createRandomUuid(),
+          });
+        } else {
+          await updateOnCache(`session:${cookies.sid}`, {
+            ...foundSession,
+            expiresIn: createExpirationTime(),
+            refreshToken,
+          });
+        }
 
-		router.put(
-			"/access-code",
-			createRateLimit(15 * 60 * 1000, 5),
-			async (request: Request, response: Response) => {
-				const { data: schemaArgs, error: schemaErrors } =
-					updateUserAccessCodeByUsernameOrEmailOrPhoneNumberSchema.safeParse({
-						body: request.body,
-					});
+        return void response.status(statusCode).json({
+          id: args?.id,
+          updatedAt: args?.updatedAt,
+        });
+      }
+    );
 
-				if (schemaErrors !== undefined) {
-					return void response
-						.status(400)
-						.json({ errors: schemaErrors.issues });
-				}
+    router.put(
+      "/access-code",
+      createRateLimit(15 * 60 * 1000, 5),
+      async (request: Request, response: Response) => {
+        const { data: schemaArgs, error: schemaErrors } =
+          updateUserAccessCodeByUsernameOrEmailOrPhoneNumberSchema.safeParse({
+            body: request.body,
+          });
 
-				const { updateUserAccessCodeByUsernameOrEmailOrPhoneNumber } =
-					UpdateUserAccessCodeByUsernameOrEmailOrPhoneNumberUseCase();
+        if (schemaErrors !== undefined) {
+          return void response
+            .status(400)
+            .json({ errors: schemaErrors.issues });
+        }
 
-				const { statusCode, args } =
-					await updateUserAccessCodeByUsernameOrEmailOrPhoneNumber({
-						schemaArgs,
-					});
+        const { updateUserAccessCodeByUsernameOrEmailOrPhoneNumber } =
+          UpdateUserAccessCodeByUsernameOrEmailOrPhoneNumberUseCase();
 
-				return void response.status(statusCode).json({
-					id: args?.id,
-					updatedAt: args?.updatedAt,
-				});
-			},
-		);
+        const { statusCode, args } =
+          await updateUserAccessCodeByUsernameOrEmailOrPhoneNumber({
+            schemaArgs,
+          });
 
-		router.delete(
-			"/:id/sessions",
-			async (request: Request, response: Response) => {
-				const { data: schemaArgs, error: schemaErrors } =
-					deleteUserSessionByIdSchema.safeParse({ params: request.params });
+        return void response.status(statusCode).json({
+          id: args?.id,
+          updatedAt: args?.updatedAt,
+        });
+      }
+    );
 
-				if (schemaErrors !== undefined) {
-					return void response
-						.status(400)
-						.json({ errors: schemaErrors.issues });
-				}
+    router.delete(
+      "/:id/sessions",
+      async (request: Request, response: Response) => {
+        const { data: schemaArgs, error: schemaErrors } =
+          deleteUserSessionByIdSchema.safeParse({ params: request.params });
 
-				const { deleteUserSessionById } = DeleteUserSessionByIdUseCase();
-				const { statusCode } = await deleteUserSessionById({
-					schemaArgs,
-				});
+        if (schemaErrors !== undefined) {
+          return void response
+            .status(400)
+            .json({ errors: schemaErrors.issues });
+        }
 
-				const { getSession, clearSession } = SessionProvider(request, response);
+        const { deleteUserSessionById } = DeleteUserSessionByIdUseCase();
+        const { statusCode } = await deleteUserSessionById({
+          schemaArgs,
+        });
 
-				const session = getSession();
+        const { getSession, clearSession } = SessionProvider(request, response);
 
-				if (session && session.sid !== undefined) {
-					const { removeFromCache } = CacheService();
-					await removeFromCache(`session:${session.sid}`);
-					clearSession();
-				}
+        const session = getSession();
 
-				return void response.status(statusCode).json();
-			},
-		);
+        if (session && session.sid !== undefined) {
+          const { removeFromCache } = CacheService();
+          await removeFromCache(`session:${session.sid}`);
+          clearSession();
+        }
 
-		return router;
-	};
+        return void response.status(statusCode).json();
+      }
+    );
 
-	return { subscribe };
+    return router;
+  };
+
+  return { subscribe };
 };
 
 export { UserRouter };
