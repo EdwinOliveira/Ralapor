22992918cc2ea2775e36014b595f2967944390b0
Diogo Oliveira <S02666@newday.co.uk>
2025-06-08 17:26:14 +0100

    WIP: Updated code around cache

--- packages/backend/src/guards/SessionGuard.ts
+++ packages/backend/src/guards/SessionGuard.ts
@@ -5,84 +5,92 @@ import { TokenProvider } from "../providers/TokenProvider";
 import { RandomProvider } from "../providers/RandomProvider";
 
 const SessionGuard = () => {
-	return {
-		isAuthenticated: async (
-			request: Request,
-			response: Response,
-			next: NextFunction,
-		) => {
-			try {
-				const { getSession, clearSession } = SessionProvider(request, response);
-				const session = getSession();
+  return {
+    isAuthenticated: async (
+      request: Request,
+      response: Response,
+      next: NextFunction
+    ) => {
+      try {
+        const { getSession, clearSession } = SessionProvider(request, response);
+        const session = getSession();
 
-				if (session && session.sid !== undefined) {
-					const { findOnCache, updateOnCache, removeFromCache } =
-						CacheService();
-					const foundSession = await findOnCache(`session:${session.sid}`);
+        if (session && session.sid !== undefined) {
+          const {
+            findOnCache,
+            updateOnCache,
+            removeFromCache,
+            isSessionCache,
+          } = CacheService();
+          const foundSession = await findOnCache(`session:${session.sid}`);
 
-					if (foundSession !== undefined) {
-						if (Date.now() <= foundSession.expiresIn) {
-							return void next();
-						}
+          if (isSessionCache(foundSession)) {
+            if (Date.now() <= foundSession.expiresIn) {
+              return void next();
+            }
 
-						const tokenData = await TokenProvider().checkToken(
-							foundSession.refreshToken,
-							process.env.SESSION_TOKEN_SECRET,
-						);
+            const tokenData = await TokenProvider().checkToken(
+              foundSession.refreshToken,
+              process.env.SESSION_TOKEN_SECRET
+            );
 
-						if (tokenData.signature === "TokenSuccess") {
-							const { createExpirationTime } = RandomProvider();
-							const expirationTime = createExpirationTime();
+            if (tokenData.signature === "TokenSuccess") {
+              const { createExpirationTime } = RandomProvider();
+              const expirationTime = createExpirationTime();
 
-							await updateOnCache(`session:${foundSession.sessionId}`, {
-								sessionId: foundSession.sessionId,
-								userId: foundSession.userId,
-								roleId: foundSession.roleId,
-								expiresIn: new Date().setSeconds(expirationTime),
-								refreshToken: foundSession.refreshToken,
-								deviceUuid: foundSession.deviceUuid,
-							});
+              await updateOnCache(`session:${foundSession.sessionId}`, {
+                sessionId: foundSession.sessionId,
+                userId: foundSession.userId,
+                roleId: foundSession.roleId,
+                expiresIn: new Date().setSeconds(expirationTime),
+                refreshToken: foundSession.refreshToken,
+                deviceUuid: foundSession.deviceUuid,
+              });
 
-							return void next();
-						}
-					}
+              return void next();
+            }
+          }
 
-					await removeFromCache(`session:${session.sid}`);
-					clearSession();
-				}
+          await removeFromCache(`session:${session.sid}`);
+          clearSession();
+        }
 
-				return void response.status(401).json();
-			} catch (error) {
-				return void response.status(500).json();
-			}
-		},
-		isAuthenticating: async (
-			request: Request,
-			response: Response,
-			next: NextFunction,
-		) => {
-			try {
-				const { getSession, clearSession } = SessionProvider(request, response);
-				const session = getSession();
+        return void response.status(401).json();
+      } catch (error) {
+        return void response.status(500).json();
+      }
+    },
+    isAuthenticating: async (
+      request: Request,
+      response: Response,
+      next: NextFunction
+    ) => {
+      try {
+        const { getSession, clearSession } = SessionProvider(request, response);
+        const session = getSession();
 
-				if (session && session.sid !== undefined) {
-					const { findOnCache, removeFromCache } = CacheService();
-					const foundSession = await findOnCache(`session:${session.sid}`);
+        if (session && session.sid !== undefined) {
+          const { findOnCache, removeFromCache, isSessionCache } =
+            CacheService();
+          const foundSession = await findOnCache(`session:${session.sid}`);
 
-					if (foundSession && Date.now() <= foundSession.expiresIn) {
-						return void response.status(409).json();
-					}
+          if (
+            isSessionCache(foundSession) &&
+            Date.now() <= foundSession.expiresIn
+          ) {
+            return void response.status(409).json();
+          }
 
-					await removeFromCache(`session:${session.sid}`);
-					clearSession();
-				}
+          await removeFromCache(`session:${session.sid}`);
+          clearSession();
+        }
 
-				return void next();
-			} catch (error) {
-				return void response.status(500).json();
-			}
-		},
-	};
+        return void next();
+      } catch (error) {
+        return void response.status(500).json();
+      }
+    },
+  };
 };
 
 export { SessionGuard };
--- packages/backend/src/services/CacheService.ts
+++ packages/backend/src/services/CacheService.ts
@@ -3,46 +3,63 @@ import { Client } from "memjs";
 type CacheData = CacheSession | CacheMFACode;
 
 type CacheSession = {
-	sessionId: string;
-	userId: number;
-	roleId: number;
-	expiresIn: number;
-	refreshToken: string;
-	deviceUuid: string;
+  sessionId: string;
+  userId: number;
+  roleId: number;
+  expiresIn: number;
+  refreshToken: string;
+  deviceUuid: string;
 };
 
 type CacheMFACode = {
-	code: string;
+  code: string;
 };
 
 const CacheService = () => {
-	const cache = Client.create("", { expires: 3600 });
+  const cache = Client.create("", { expires: 3600 });
 
-	const findOnCache = async (property: string) => {
-		const { value: cachedData } = await cache.get(property);
+  const findOnCache = async (property: string) => {
+    const { value: cachedData } = await cache.get(property);
 
-		if (cachedData) {
-			return JSON.parse(cachedData.toString()) as CacheData;
-		}
-	};
+    if (cachedData) {
+      return JSON.parse(cachedData.toString()) as CacheData;
+    }
+  };
 
-	const addToCache = async (
-		property: string,
-		data: CacheData,
-		expiresIn?: number,
-	) => {
-		cache.add(property, JSON.stringify(data), { expires: expiresIn });
-	};
+  const isSessionCache = (
+    cache: CacheData | undefined
+  ): cache is CacheSession => {
+    return !!(cache && "sessionId" in cache);
+  };
 
-	const updateOnCache = async (property: string, data: CacheData) => {
-		cache.replace(property, JSON.stringify(data));
-	};
+  const isMFACache = (cache: CacheData | undefined): cache is CacheMFACode => {
+    return !!(cache && "code" in cache);
+  };
 
-	const removeFromCache = async (property: string) => {
-		cache.delete(property);
-	};
+  const addToCache = async (
+    property: string,
+    data: CacheData,
+    expiresIn?: number
+  ) => {
+    cache.add(property, JSON.stringify(data), { expires: expiresIn });
+  };
 
-	return { findOnCache, addToCache, updateOnCache, removeFromCache };
+  const updateOnCache = async (property: string, data: CacheData) => {
+    cache.replace(property, JSON.stringify(data));
+  };
+
+  const removeFromCache = async (property: string) => {
+    cache.delete(property);
+  };
+
+  return {
+    findOnCache,
+    isSessionCache,
+    isMFACache,
+    addToCache,
+    updateOnCache,
+    removeFromCache,
+  };
 };
 
 export { CacheService };
