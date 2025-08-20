87c24d1f2a465b0587c8db3380e98da55a502e74
Diogo Oliveira <S02666@newday.co.uk>
2025-08-20 10:40:50 +0100

    feat(main): added router for users

--- packages/backend/src/routers/UserRouter.ts
+++ packages/backend/src/routers/UserRouter.ts
@@ -1,8 +1,18 @@
-import { Router } from 'express';
+import { type Request, type Response, Router } from 'express';
+
+import { CreateUserUseCase } from '../useCases/users/create-user';
 
 const UserRouter = () => {
+  const createUser = async (request: Request, response: Response) => {
+    const { createUser } = CreateUserUseCase({ request, response });
+    const { data, errors, status } = await createUser();
+
+    return void response.status(status).json({ data, errors });
+  };
+
   return {
     subscribe: (router: Router) => {
+      router.post('/', createUser);
       return router;
     },
   };
